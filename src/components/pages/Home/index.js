479fca52537682b4a1f9277e5404cc21b272b058
zhangwu-engineer <zhangu.engineer@gmail.com>
2020-03-24 04:39:14 -0400

    Home page to handle Okta authentication

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -1,51 +1,39 @@
-import { useOktaAuth } from '@okta/okta-react'
-import React, { useState, useEffect } from 'react'
-import { useHistory } from 'react-router-dom'
+import React, { Component } from 'react'
 import { Spinner } from 'react-bootstrap'
-import { defaultRoute } from '../../../routes'
+import { compose } from 'redux'
+import { withOktaAuth } from '@okta/okta-react'
+import { withRouter } from 'react-router-dom'
 import Signin from '../Signin'
-import { titleTemplate } from '../../../routes'
+import { titleTemplate, defaultRoute } from '../../../routes'
 
-const Home = () => {
-  const { authState, authService } = useOktaAuth()
-  const [userInfo, setUserInfo] = useState(null)
-  const history = useHistory()
+class Home extends Component {
 
-  useEffect(() => {
-    if (!authState.isAuthenticated) {
-      // When user isn't authenticated, forget any user info
-      setUserInfo(null)
-    } else {
-      authService.getUser().then((info) => {
-        setUserInfo(info)
-      })
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      userInfo: null
     }
-  }, [authState, authService]) // Update if authState changes
-
-  const login = async () => {
-    authService.login('/')
+    this.login = this.login.bind(this);
   }
 
-  if (authState.isPending) {
-    return (
-      <div className="layout-inner d-flex justify-content-center align-items-center pb-2 mb-2">
-        <div className="ui-w-60">
-          <div className="w-100 position-relative">
-            <Spinner animation="border" size="ls" variant="primary" />
-          </div>
-        </div>
-      </div>
-    )
+  async componentDidMount() {
+    await this.props.authService.getUser().then((userInfo) => {
+      this.setState({ userInfo });
+    });
   }
 
-  if (authState.isAuthenticated && userInfo) {
-    history.push(defaultRoute)
+  async login() {
+    this.props.authService.login('/');
   }
 
-  return (
-    <div>
-      { authState.isAuthenticated && !userInfo
-      && (
+  render() {
+
+    const { authState } = this.props
+    const { userInfo } = this.state
+
+    if (authState.isPending) {
+      return (
         <div className="layout-inner d-flex justify-content-center align-items-center pb-2 mb-2">
           <div className="ui-w-60">
             <div className="w-100 position-relative">
@@ -53,19 +41,39 @@ const Home = () => {
             </div>
           </div>
         </div>
-      )}
-
-      {!authState.isAuthenticated
-      && (
+      )
+    }
+  
+    if (authState.isAuthenticated && userInfo) {
+      this.props.history.push(defaultRoute)
+    }
+  
+    return (
       <div>
-        <Signin
-          onLogin={login}
-          onSetTitle={e => document.title = titleTemplate.replace('%s', e)}
-        />
+        { authState.isAuthenticated && !userInfo
+        && (
+          <div className="layout-inner d-flex justify-content-center align-items-center pb-2 mb-2">
+            <div className="ui-w-60">
+              <div className="w-100 position-relative">
+                <Spinner animation="border" size="ls" variant="primary" />
+              </div>
+            </div>
+          </div>
+        )}
+  
+        {!authState.isAuthenticated
+        && (
+        <div>
+          <Signin
+            onLogin={this.login}
+            onSetTitle={e => document.title = titleTemplate.replace('%s', e)}
+          />
+        </div>
+        )}
       </div>
-      )}
-    </div>
-  )
+    )
+  }
+
 }
 
-export default Home
+export default compose(withRouter, withOktaAuth)(Home)
