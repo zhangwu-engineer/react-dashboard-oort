c04bc70423995a955138556c961fde4a4e6d50f8
Zhang Wu <zhangwu.engineer@gmail.com>
2020-03-24 12:34:14 -0400

    Layout update

--- src/components/pages/Projects/ProjectDetails/index.js
+++ src/components/pages/Projects/ProjectDetails/index.js
@@ -43,28 +43,18 @@ class ProjectsItem extends Component {
         completedTasks: 29,
 
         description: `
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque magna augue, euismod at tortor et, laoreet maximus risus. Ut neque felis, luctus ut rhoncus id, elementum vitae lorem. Ut ac turpis sit amet lorem volutpat tincidunt. Vestibulum dui sapien, porttitor eget pellentesque id, ultrices id ipsum. Nam augue mi, maximus ut tortor et, fermentum efficitur diam. Suspendisse eget urna lorem. Fusce ligula augue, malesuada ullamcorper est nec, commodo laoreet tellus.</p>
-  <p>Proin imperdiet purus et lectus luctus, rutrum fermentum lorem molestie. Nam urna felis, elementum elementum placerat id, fermentum ac urna. Etiam libero sem, porttitor ultrices efficitur condimentum, lobortis sit amet enim. Sed vitae orci nulla.</p>
-  <p>Cras ultrices, dui id vulputate laoreet, diam orci semper ipsum, a aliquet nunc quam vitae turpis. Donec cursus tortor nec turpis semper, ac luctus mauris sagittis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
-  <p>Aliquam ornare nisl semper nisl porttitor commodo vel vel libero. Pellentesque faucibus, nisl vel luctus porttitor, leo felis pellentesque augue, dignissim tempus risus odio sed lorem. Nunc nec malesuada nunc, ut mollis dui. Vestibulum ac lacinia massa. Phasellus eget purus nisi. Quisque sodales, tortor et elementum dapibus, nisl urna hendrerit metus, a rhoncus magna sem in libero. Aliquam mattis erat ut diam viverra, quis iaculis sem pulvinar. Vivamus ut mi vitae arcu finibus convallis. Nulla turpis lacus, tempor et sagittis non, egestas nec ante.</p>
-  <p>Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque pharetra turpis non aliquet ornare. Duis euismod ultricies est sed auctor. Sed luctus accumsan enim ut efficitur.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque magna augue, euismod at tortor et, laoreet maximus risus. Ut neque felis, luctus ut rhoncus id, elementum vitae lorem. Ut ac turpis sit amet lorem volutpat tincidunt. Vestibulum dui sapien, porttitor eget pellentesque id, ultrices id ipsum. Nam augue mi, maximus ut tortor et, fermentum efficitur diam. Suspendisse eget urna lorem. Fusce ligula augue, malesuada ullamcorper est nec, commodo laoreet tellus.</p>
+          <p>Proin imperdiet purus et lectus luctus, rutrum fermentum lorem molestie. Nam urna felis, elementum elementum placerat id, fermentum ac urna. Etiam libero sem, porttitor ultrices efficitur condimentum, lobortis sit amet enim. Sed vitae orci nulla.</p>
+          <p>Cras ultrices, dui id vulputate laoreet, diam orci semper ipsum, a aliquet nunc quam vitae turpis. Donec cursus tortor nec turpis semper, ac luctus mauris sagittis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
         `,
 
-        created: '02/01/2018',
+        createdAt: '02/01/2018',
         lastUpdate: '02/18/2018',
-        deadline: '03/12/2018',
-
-        hourRate: 35,
-        hours: 74,
-
-        tags: ['Development', 'Frontend', 'Backend', 'Design'],
+        startDate: '03/12/2018',
+        endDate: '04/12/2018',
 
         createdBy: {
           name: 'Mae Gibson'
-        },
-
-        client: {
-          name: 'Company Ltd.'
         },
 
         team: [
@@ -126,16 +116,6 @@ class ProjectsItem extends Component {
     }
   }
 
-  priorityDropdownVariant (priority) {
-    let variant
-
-    if (priority === 1) variant = 'danger'
-    if (priority === 2) variant = 'success'
-    if (priority === 3) variant = 'warning'
-
-    return `${variant} btn-xs md-btn-flat`
-  }
-
   prevent(e) {
     e.preventDefault()
   }
@@ -157,22 +137,51 @@ class ProjectsItem extends Component {
           </Col>
           <Col md={4} xl={3}>
             <h5 className="py-3 mb-4 t-align-right">
-              <Badge variant={`outline-primary font-weight-normal px-3 py-2`}>Completed</Badge>
+              <Badge variant={`outline-primary font-weight-normal px-3 py-2`}>{this.state.statuses[this.state.projectData.status].title}</Badge>
             </h5>
           </Col>
         </Row>
 
         <Row>
-          <Col>
+          <Col md={4} xl={3}>
 
-            {/* Project Image */}
+            {/* Project details */}
             <Card className="mb-4">
-              <Card.Header as="h6">Project Image</Card.Header>
-              <Card.Body>
-                <Carousel views={images2} currentIndex={this.state.photoIndex2} />
-              </Card.Body>
+              <Card.Header as="h6">Project details</Card.Header>
+              <ListGroup variant="flush">
+                <ListGroup.Item className="d-flex justify-content-between align-items-center">
+                  <Carousel views={images2} currentIndex={this.state.photoIndex2} />
+                </ListGroup.Item>
+                <ListGroup.Item className="d-flex justify-content-between align-items-center">
+                  <div className="text-muted">Owner</div>
+                  <div><a href="#d" onClick={this.prevent}>{this.state.projectData.createdBy.name}</a></div>
+                </ListGroup.Item>
+                <ListGroup.Item className="d-flex justify-content-between align-items-center">
+                  <div className="text-muted">Created at</div>
+                  <div>{this.state.projectData.createdAt}</div>
+                </ListGroup.Item>
+                <ListGroup.Item className="d-flex justify-content-between align-items-center">
+                  <div className="text-muted">Modified at</div>
+                  <div>{this.state.projectData.lastUpdate}</div>
+                </ListGroup.Item>
+                <ListGroup.Item className="d-flex justify-content-between align-items-center">
+                  <div className="text-muted">Start Date</div>
+                  <div>{this.state.projectData.startDate}</div>
+                </ListGroup.Item>
+                <ListGroup.Item className="d-flex justify-content-between align-items-center">
+                  <div className="text-muted">End Date</div>
+                  <div>{this.state.projectData.endDate}</div>
+                </ListGroup.Item>
+                <ListGroup.Item className="d-flex justify-content-between align-items-center">
+                  <div className="text-muted">Status</div>
+                  <Badge variant={`outline-primary font-weight-normal px-3 py-2`}>{this.state.statuses[this.state.projectData.status].title}</Badge>
+                </ListGroup.Item>
+              </ListGroup>
             </Card>
-            {/* / Project Image */}
+            {/* / Project details */}
+
+          </Col>
+          <Col>
 
             {/* Description */}
             <Card className="mb-4">
@@ -252,74 +261,7 @@ class ProjectsItem extends Component {
             </div>
 
           </Col>
-          <Col md={4} xl={3}>
-
-            {/* Project details */}
-            <Card className="mb-4">
-              <Card.Header as="h6">Project details</Card.Header>
-              <ListGroup variant="flush">
-                <ListGroup.Item className="d-flex justify-content-between align-items-center">
-                  <div className="text-muted">Client</div>
-                  <div><a href="#d" onClick={this.prevent}>{this.state.projectData.client.name}</a></div>
-                </ListGroup.Item>
-                <ListGroup.Item className="d-flex justify-content-between align-items-center">
-                  <div className="text-muted">Created by</div>
-                  <div><a href="#d" onClick={this.prevent}>{this.state.projectData.createdBy.name}</a></div>
-                </ListGroup.Item>
-                <ListGroup.Item className="d-flex justify-content-between align-items-center">
-                  <div className="text-muted">Created at</div>
-                  <div>{this.state.projectData.created}</div>
-                </ListGroup.Item>
-                <ListGroup.Item className="d-flex justify-content-between align-items-center">
-                  <div className="text-muted">Last update</div>
-                  <div>{this.state.projectData.lastUpdate}</div>
-                </ListGroup.Item>
-                <ListGroup.Item className="d-flex justify-content-between align-items-center">
-                  <div className="text-muted">Deadline</div>
-                  <div><strong>{this.state.projectData.deadline}</strong></div>
-                </ListGroup.Item>
-                <ListGroup.Item className="d-flex justify-content-between align-items-center">
-                  <div className="text-muted">Priority</div>
-                  <DropdownButton
-                    variant={this.priorityDropdownVariant(this.state.projectData.priority)}
-                    title={<React.Fragment>
-                      {this.state.projectData.priority === 1 && <span>High</span>}
-                      {this.state.projectData.priority === 2 && <span>Medium</span>}
-                      {this.state.projectData.priority === 3 && <span>Low</span>}
-                    </React.Fragment>}
-                    alignRight={!isRTL}
-                    key={this.state.projectData.priority}
-                  >
-                    <Dropdown.Item active={this.state.projectData.priority === 1 ? 'true' : undefined}>High</Dropdown.Item>
-                    <Dropdown.Item active={this.state.projectData.priority === 2 ? 'true' : undefined}>Medium</Dropdown.Item>
-                    <Dropdown.Item active={this.state.projectData.priority === 3 ? 'true' : undefined}>Low</Dropdown.Item>
-                  </DropdownButton>
-                </ListGroup.Item>
-                <ListGroup.Item className="d-flex justify-content-between align-items-center">
-                  <div className="text-muted">Hour rate</div>
-                  <div>${this.state.projectData.hourRate}/h</div>
-                </ListGroup.Item>
-                <ListGroup.Item className="d-flex justify-content-between align-items-center">
-                  <div className="text-muted">Total hours</div>
-                  <div>{this.state.projectData.hours}</div>
-                </ListGroup.Item>
-                <ListGroup.Item className="d-flex justify-content-between align-items-center">
-                  <div className="text-muted">Total cost</div>
-                  <div><strong>${this.state.projectData.hourRate * this.state.projectData.hours}</strong></div>
-                </ListGroup.Item>
-                {this.state.projectData.tags && this.state.projectData.tags.length && <ListGroup.Item>
-                  <div className="text-muted small">Tags</div>
-                  <div className="d-flex flex-wrap">
-                    {this.state.projectData.tags.map(tag =>
-                      <Badge key={tag} variant="default" className="mt-1 mr-1">{tag}</Badge>
-                    )}
-                  </div>
-                </ListGroup.Item>}
-              </ListGroup>
-            </Card>
-            {/* / Project details */}
-
-          </Col>
+          
         </Row>
 
       </div>
