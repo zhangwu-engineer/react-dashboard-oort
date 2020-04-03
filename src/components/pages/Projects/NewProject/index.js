f627080635f22302cda329e0188cea2af7261f76
Zhang Wu <zhangwu.engineer@gmail.com>
2020-04-03 05:44:10 -0400

    NewProject page component

--- src/components/pages/Projects/NewProject/index.js
+++ src/components/pages/Projects/NewProject/index.js
@@ -1,10 +1,52 @@
 import React, { Component } from 'react'
-import { Form, Button, Row, Col, InputGroup, Card } from 'react-bootstrap'
+import { Form, Button, Row, Col, Card } from 'react-bootstrap'
+import DatePicker from 'react-datepicker'
+import Select from 'react-select';
+import moment from 'moment'
+import FileUpload from '../../../../components/forms/FileUpload'
+import '../../../../vendor/libs/react-datepicker/react-datepicker.scss'
+
+const USERS_LIST = [
+  { value: 'John Doe', label: 'John Doe' },
+  { value: 'Trevor Virtue', label: 'Trevor Virtue' },
+  { value: 'Daly Harry', label: 'Daly Harry' },
+  { value: 'Peter Stanbridge', label: 'Peter Stanbridge' },
+  { value: 'Andrew Kazantzis', label: 'Andrew Kazantzis' },
+];
+
+const APPLICATIONS_LIST = [
+  { value: 'app1', label: 'Application 1' },
+  { value: 'app2', label: 'Application 2' },
+  { value: 'app3', label: 'Application 3' },
+  { value: 'app4', label: 'Application 4' },
+  { value: 'app5', label: 'Application 5' },
+  { value: 'app6', label: 'Application 6' },
+  { value: 'app7', label: 'Application 7' },
+  { value: 'app8', label: 'Application 8' },
+  { value: 'app9', label: 'Application 9' },
+  { value: 'app10', label: 'Application 10' },
+];
 
 class NewProject extends Component {
   constructor(props) {
     super(props)
     props.setTitle('New Project')
+
+    this.state = {
+
+      startDate: new Date(),
+      endDate: moment().add(5, 'days').toDate(),
+      created: new Date(),
+      lastActivity: moment().add(5, 'days').toDate(),
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(field, value) {
+    this.setState({
+      [field]: value
+    })
   }
 
   render() {
@@ -18,47 +60,126 @@ class NewProject extends Component {
           <Card.Header as="h6">Project Info</Card.Header>
           <Card.Body>
             <Form>
-              <Form.Group as={Row}>
-                <Form.Label column sm={2} className="text-sm-right">Name</Form.Label>
-                <Col sm={10}>
+              <Form.Row>
+                <Form.Group as={Col} md={6}>
+                  <Form.Label>Name</Form.Label>
                   <Form.Control placeholder="Project Name" />
-                </Col>
-              </Form.Group>
-              <Form.Group as={Row}>
-                <Form.Label column sm={2} className="text-sm-right">Owner</Form.Label>
-                <Col sm={10}>
+                </Form.Group>
+                <Form.Group as={Col} md={6}>
+                  <Form.Label>Owner</Form.Label>
                   <Form.Control placeholder="Project Owner" />
-                </Col>
-              </Form.Group>
-              <Form.Group as={Row}>
-                <Form.Label column sm={2} className="text-sm-right">Textarea</Form.Label>
-                <Col sm={10}>
-                  <Form.Control as="textarea" placeholder="Password" />
-                </Col>
-              </Form.Group>
-              <Form.Group as={Row}>
-                <Form.Label column sm={2} className="text-sm-right pt-0">Radios</Form.Label>
-                <Col sm={10}>
-                  <Form.Check defaultChecked custom type="radio" name="form-horizontal-radios-example" id="form-horizontal-radios-example-1"
-                    label="Option one is this and that&mdash;be sure to include why it's great" />
-                  <Form.Check custom type="radio" name="form-horizontal-radios-example" id="form-horizontal-radios-example-2"
-                    label="Option two can be something else and selecting it will deselect option one" />
-                  <Form.Check custom disabled type="radio" name="form-horizontal-radios-example" id="form-horizontal-radios-example-3"
-                    label="Option three is disabled" />
-                </Col>
-              </Form.Group>
-              <Form.Group as={Row}>
-                <Form.Label column sm={2} className="text-sm-right pt-0">Checkbox</Form.Label>
-                <Col sm={10}>
-                  <Form.Check custom type="checkbox" name="form-horizontal-radios-example" id="form-horizontal-checkbox-example-1"
-                    label="Check me out" />
-                </Col>
-              </Form.Group>
-              <Form.Group as={Row}>
-                <Col sm={{ span: 10, offset: 2 }}>
+                </Form.Group>
+              </Form.Row>
+              <Form.Row>
+                <Form.Group as={Col} md={12}>
+                  <Form.Label>Description</Form.Label>
+                  <Form.Control as="textarea" placeholder="Project description" />
+                </Form.Group>
+              </Form.Row>
+              <Form.Row>
+                <Form.Group as={Col} md={4}>
+                  <Form.Label>Status</Form.Label>
+                  <select className="custom-select">
+                    <option>Select status</option>
+                    <option>Active</option>
+                    <option>Pending</option>
+                    <option>Completed</option>
+                    <option>Cancelled</option>
+                  </select>
+                </Form.Group>
+                <Form.Group as={Col} md={4}>
+                  <Form.Label>Risk Score</Form.Label>
+                  <Form.Control type="number" placeholder="Risk Score" />
+                </Form.Group>
+                <Form.Group as={Col} md={4}>
+                  <Form.Label>Risk Description</Form.Label>
+                  <Form.Control placeholder="Risk Description" />
+                </Form.Group>
+              </Form.Row>
+              <Form.Row>
+                <Form.Group as={Col} md={3}>
+                  <Form.Label>Created</Form.Label>
+                  <DatePicker className="form-control"
+                    calendarClassName = "react-datepicker--with-time"
+                    selected={this.state.created}
+                    onChange={e => this.handleChange('created', e)}
+                    showTimeSelect
+                    timeFormat="HH:mm"
+                    timeIntervals={15}
+                    dateFormat="MMMM d, yyyy h:mm aa"
+                    timeCaption="Time"
+                    todayButton={"Today"}
+                  />
+                </Form.Group>
+                <Form.Group as={Col} md={3}>
+                  <Form.Label>Last Activity</Form.Label>
+                  <DatePicker className="form-control"
+                    calendarClassName = "react-datepicker--with-time"
+                    selected={this.state.lastActivity}
+                    onChange={e => this.handleChange('lastActivity', e)}
+                    showTimeSelect
+                    timeFormat="HH:mm"
+                    timeIntervals={15}
+                    dateFormat="MMMM d, yyyy h:mm aa"
+                    timeCaption="Time"
+                    todayButton={"Today"}
+                  />
+                </Form.Group>
+                <Form.Group as={Col} md={3}>
+                  <Form.Label>Start Date</Form.Label>
+                  <DatePicker className="form-control"
+                    calendarClassName = "react-datepicker--with-time"
+                    selected={this.state.startDate}
+                    onChange={e => this.handleChange('startDate', e)}
+                    showTimeSelect
+                    timeFormat="HH:mm"
+                    timeIntervals={15}
+                    dateFormat="MMMM d, yyyy h:mm aa"
+                    timeCaption="Time"
+                    todayButton={"Today"}
+                  />
+                </Form.Group>
+                <Form.Group as={Col} md={3}>
+                  <Form.Label>End date</Form.Label>
+                  <DatePicker className="form-control"
+                    calendarClassName = "react-datepicker--with-time"
+                    selected={this.state.endDate}
+                    onChange={e => this.handleChange('endDate', e)}
+                    showTimeSelect
+                    timeFormat="HH:mm"
+                    timeIntervals={15}
+                    dateFormat="MMMM d, yyyy h:mm aa"
+                    timeCaption="Time"
+                    todayButton={"Today"}
+                  />
+                </Form.Group>
+              </Form.Row>
+              <Form.Row>
+                <Form.Group as={Col} md={12}>
+                  <Form.Label>Users</Form.Label>
+                  <Select isMulti className="react-select" classNamePrefix="react-select"
+                    options={USERS_LIST}
+                    defaultValue={this.state.multiValue} />
+                </Form.Group>
+              </Form.Row>
+              <Form.Row>
+                <Form.Group as={Col} md={12}>
+                  <Form.Label>Applications</Form.Label>
+                  <Select isMulti className="react-select" classNamePrefix="react-select"
+                    options={APPLICATIONS_LIST}
+                    defaultValue={this.state.multiValue} />
+                </Form.Group>
+              </Form.Row>
+              <Form.Row className="mt-3">
+                <Form.Group as={Col} md={12}>
+                  <FileUpload />
+                </Form.Group>
+              </Form.Row>
+              <Form.Row className="mt-3">
+                <Form.Group as={Col} md={12}>
                   <Button type="submit" variant="primary">Submit</Button>
-                </Col>
-              </Form.Group>
+                </Form.Group>
+              </Form.Row>
             </Form>
           </Card.Body>
         </Card>
