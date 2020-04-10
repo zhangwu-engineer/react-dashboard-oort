680d1d4f7eb1d62ca024022fc0b757640edd22d3
zhangwu-engineer <zhangu.engineer@gmail.com>
2020-04-10 07:46:52 -0400

    redux - getProject

--- src/api/projects.js
+++ src/api/projects.js
@@ -10,3 +10,15 @@ export const loadProjects = async () => {
   })
   return response.data
 }
+
+export const getProject = async (id) => {
+  const response = await axios({
+    method: 'get',
+    url: 'https://staging.ctrl.oort.io/projects/'+id,
+    headers: {
+      Authorization: 'Basic c3RhZ2luZzoybnR0aFB2Tg=='
+    }
+  })
+  return response.data
+}
+
--- src/store/actions/projects.js
+++ src/store/actions/projects.js
@@ -1,5 +1,9 @@
-import { GET_PROJECTS_REQUESTED } from '../constants/action-types'
+import { GET_PROJECTS_REQUESTED, GET_PROJECT_REQUESTED } from '../constants/action-types'
 
 export function getProjects() {
   return { type: GET_PROJECTS_REQUESTED }
+}
+
+export function getProject(id) {
+  return { type: GET_PROJECT_REQUESTED, id }
 }
