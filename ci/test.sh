fd13b264d3c4b9c9a65c5667a655bfe225e83e36
Alan Potter <alanjpotter@gmail.com>
2020-04-14 05:19:06 -0400

    Don't expect a database to be part of deployment
    `.gitlab-ci.yml` was still expecting a database to be part of a
    deployment. These changes remove that expectation.
    
    Also fixes the value of REACT_APP_OKTA_ISSUER.

--- ci/test.sh
+++ ci/test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --rm -e CI=true --entrypoint npm $UI_IMAGE:$UI_IMAGE_TAG run test -- "$@"
+docker run --rm -e CI=true --entrypoint npm $IMAGE:$IMAGE_TAG run test -- "$@"
