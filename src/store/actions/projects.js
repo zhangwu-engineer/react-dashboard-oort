e85be75506493ac0ead08b1b14d01b8e3e643bf0
Zhang Wu <zhangwu.engineer@gmail.com>
2020-04-14 16:38:11 -0400

    project create/update redux workflow

--- src/store/actions/projects.js
+++ src/store/actions/projects.js
@@ -1,4 +1,10 @@
-import { GET_PROJECTS_REQUESTED, GET_PROJECT_REQUESTED, DELETE_PROJECT_REQUESTED } from '../constants/action-types'
+import {
+  GET_PROJECTS_REQUESTED,
+  GET_PROJECT_REQUESTED,
+  DELETE_PROJECT_REQUESTED,
+  CREATE_PROJECT_REQUESTED,
+  UPDATE_PROJECT_REQUESTED
+} from '../constants/action-types'
 
 export function getProjects() {
   return { type: GET_PROJECTS_REQUESTED }
@@ -10,4 +16,12 @@ export function getProject(id) {
 
 export function deleteProject(id) {
   return { type: DELETE_PROJECT_REQUESTED, id }
+}
+
+export function createProject(project) {
+  return { type: CREATE_PROJECT_REQUESTED, project }
+}
+
+export function updateProject(project) {
+  return { type: UPDATE_PROJECT_REQUESTED, project }
 }
--- src/store/saga/projects.js
+++ src/store/saga/projects.js
@@ -5,6 +5,8 @@ export default function* () {
   yield takeEvery("GET_PROJECTS_REQUESTED", getProjectsRequested)
   yield takeEvery("GET_PROJECT_REQUESTED", getProjectRequested)
   yield takeEvery("DELETE_PROJECT_REQUESTED", deleteProjectRequested)
+  yield takeEvery("CREATE_PROJECT_REQUESTED", createProjectRequested)
+  yield takeEvery("UPDATE_PROJECT_REQUESTED", updateProjectRequested)
 }
 
 function* getProjectsRequested() {
@@ -31,6 +33,24 @@ function* deleteProjectRequested({ id }) {
     yield put({ type: "DELETE_PROJECT_SUCCESS", payload })
   } catch (e) {
     yield put({ type: "DELETE_PROJECT_FAILED", payload: e })
+  }
+}
+
+function* createProjectRequested({ project }) {
+  try {
+    const payload = project
+    yield put({ type: "CREATE_PROJECT_SUCCESS", payload })
+  } catch (e) {
+    yield put({ type: "CREATE_PROJECT_FAILED", payload: e })
+  }
+}
+
+function* updateProjectRequested({ project }) {
+  try {
+    const payload = project
+    yield put({ type: "UPDATE_PROJECT_SUCCESS", payload })
+  } catch (e) {
+    yield put({ type: "UPDATE_PROJECT_FAILED", payload: e })
   }
 }
 
