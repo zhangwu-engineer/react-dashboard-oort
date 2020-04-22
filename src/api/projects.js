3b999e70147a15ae004c0f83cccda8cc9b1d5cf7
zhangwu-engineer <zhangu.engineer@gmail.com>
2020-04-22 16:32:12 -0400

    fix accessToken

--- src/api/projects.js
+++ src/api/projects.js
@@ -2,24 +2,26 @@ import axios from 'axios'
 import config from '../shared/config'
 
 export const loadProjects = async () => {
-  const sessionToken = localStorage.getItem('sessionToken')
+  const oktaTokenStorage = localStorage.getItem('okta-token-storage')
+  const accessToken = JSON.parse(oktaTokenStorage) && JSON.parse(oktaTokenStorage).accessToken.accessToken
   const response = await axios({
     method: 'get',
     url: config.PROJECTS_API_URL,
     headers: {
-      Authorization: `Basic ${sessionToken}`
+      Authorization: `Basic ${accessToken}`
     }
   })
   return response.data
 }
 
 export const getProject = async (id) => {
-  const sessionToken = localStorage.getItem('sessionToken')
+  const oktaTokenStorage = localStorage.getItem('okta-token-storage')
+  const accessToken = JSON.parse(oktaTokenStorage) && JSON.parse(oktaTokenStorage).accessToken.accessToken
   const response = await axios({
     method: 'get',
     url: `${config.PROJECTS_API_URL}/id`,
     headers: {
-      Authorization: `Basic ${sessionToken}`
+      Authorization: `Basic ${accessToken}`
     }
   })
   return response.data
