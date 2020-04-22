14f34cb5f9ae14a12bbe65510afde3c0492b90bd
zhangwu-engineer <zhangu.engineer@gmail.com>
2020-04-22 12:50:07 -0400

    store Okta sessionToken into localStorage after sign in

--- src/components/pages/Signin/SigninForm.js
+++ src/components/pages/Signin/SigninForm.js
@@ -29,9 +29,12 @@ class SigninForm extends Component {
       username: this.state.email,
       password: this.state.password
     })
-    .then(res => this.setState({
-      sessionToken: res.sessionToken
-    }))
+    .then(res => {
+      this.setState({
+        sessionToken: res.sessionToken
+      })
+      localStorage.setItem('sessionToken', res.sessionToken)
+    })
     .catch(err => console.log('Found an error', err));
   }
 
