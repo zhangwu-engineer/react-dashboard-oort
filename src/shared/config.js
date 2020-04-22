65d6e52b2d0697cf1859af3aeef4e7939afc261b
Zhang Wu <zhangwu.engineer@gmail.com>
2020-04-22 12:50:27 -0400

    export PROJECTS_API_URL

--- src/shared/config.js
+++ src/shared/config.js
@@ -1,11 +1,13 @@
 const ISSUER = process.env.REACT_APP_OKTA_ISSUER
 const CLIENT_ID = process.env.REACT_APP_OKTA_CLIENT_ID;
 const ACTIVITY_LOG_API = process.env.REACT_APP_ACTIVITY_LOG_API_URL;
+const PROJECTS_API_URL = process.env.REACT_APP_PROJECTS_API_URL;
 
 const config = {
   ISSUER,
   CLIENT_ID,
-  ACTIVITY_LOG_API
+  ACTIVITY_LOG_API,
+  PROJECTS_API_URL
 }
 
 export default config
