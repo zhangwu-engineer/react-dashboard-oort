7a8a2873c918037d72b4a9f5ad8a1c674f7d6a8d
zhangwu-engineer <zhangu.engineer@gmail.com>
2020-04-24 16:08:45 -0400

    signin validation

--- src/components/pages/Signin/index.js
+++ src/components/pages/Signin/index.js
@@ -1,21 +1,13 @@
 import React, { Component } from 'react';
-import { Spinner } from 'react-bootstrap'
 import { Redirect } from 'react-router-dom';
 import SigninForm from './SigninForm';
 import { withOktaAuth } from '@okta/okta-react';
+import Loader from '../../../shared/Loader'
 
 export default withOktaAuth(class Signin extends Component {
   render() {
     if (this.props.authState.isPending) { 
-      return (
-        <div className="layout-inner d-flex justify-content-center align-items-center pb-2 mb-2">
-          <div className="ui-w-60">
-            <div className="w-100 position-relative">
-              <Spinner animation="border" size="ls" variant="primary" />
-            </div>
-          </div>
-        </div>
-      );
+      return <Loader />;
     }
     return this.props.authState.isAuthenticated ?
       <Redirect to={{ pathname: '/' }}/> :
