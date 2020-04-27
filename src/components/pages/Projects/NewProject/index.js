4444ae3853a78a3dd1460649ede176358adab38d
zhangwu-engineer <zhangu.engineer@gmail.com>
2020-04-27 04:50:04 -0400

    formik validation

--- src/components/pages/Projects/NewProject/index.js
+++ src/components/pages/Projects/NewProject/index.js
@@ -3,6 +3,7 @@ import { Breadcrumb, Form, Button, Col, Card } from 'react-bootstrap'
 import DatePicker from 'react-datepicker'
 import Select from 'react-select';
 import moment from 'moment'
+import { Formik, Form as FormikForm, Field, ErrorMessage } from 'formik';
 import '../../../../vendor/libs/react-datepicker/react-datepicker.scss'
 
 const USERS_LIST = [
@@ -65,86 +66,151 @@ class NewProject extends Component {
         <Card className="mb-4">
           <Card.Header as="h6">Project Info</Card.Header>
           <Card.Body>
-            <Form>
-              <Form.Row>
-                <Form.Group as={Col} md={4}>
-                  <Form.Label>Name</Form.Label>
-                  <Form.Control placeholder="Project Name" />
-                </Form.Group>
-                <Form.Group as={Col} md={4}>
-                  <Form.Label>Owner</Form.Label>
-                  <Form.Control placeholder="Project Owner" />
-                </Form.Group>
-              </Form.Row>
-              <Form.Row>
-                <Form.Group as={Col} md={12}>
-                  <Form.Label>Description</Form.Label>
-                  <Form.Control as="textarea" placeholder="Project description" />
-                </Form.Group>
-              </Form.Row>
-              <Form.Row>
-                <Form.Group as={Col} md={4}>
-                  <Form.Label>Status</Form.Label>
-                  <select className="custom-select">
-                    <option>Select status</option>
-                    <option>Active</option>
-                    <option>Pending</option>
-                    <option>Completed</option>
-                    <option>Cancelled</option>
-                  </select>
-                </Form.Group>
-                <Form.Group as={Col} md={4}>
-                  <Form.Label>Start Date</Form.Label>
-                  <DatePicker className="form-control"
-                    calendarClassName = "react-datepicker--with-time"
-                    selected={this.state.startDate}
-                    onChange={e => this.handleChange('startDate', e)}
-                    showTimeSelect
-                    timeFormat="HH:mm"
-                    timeIntervals={15}
-                    dateFormat="MMMM d, yyyy h:mm aa"
-                    timeCaption="Time"
-                    todayButton={"Today"}
-                  />
-                </Form.Group>
-                <Form.Group as={Col} md={4}>
-                  <Form.Label>End date</Form.Label>
-                  <DatePicker className="form-control"
-                    calendarClassName = "react-datepicker--with-time"
-                    selected={this.state.endDate}
-                    onChange={e => this.handleChange('endDate', e)}
-                    showTimeSelect
-                    timeFormat="HH:mm"
-                    timeIntervals={15}
-                    dateFormat="MMMM d, yyyy h:mm aa"
-                    timeCaption="Time"
-                    todayButton={"Today"}
-                  />
-                </Form.Group>
-              </Form.Row>
-              <Form.Row>
-                <Form.Group as={Col} md={12}>
-                  <Form.Label>Users</Form.Label>
-                  <Select isMulti className="react-select" classNamePrefix="react-select"
-                    options={USERS_LIST}
-                    defaultValue={this.state.multiValue} />
-                </Form.Group>
-              </Form.Row>
-              <Form.Row>
-                <Form.Group as={Col} md={12}>
-                  <Form.Label>Applications</Form.Label>
-                  <Select isMulti className="react-select" classNamePrefix="react-select"
-                    options={APPLICATIONS_LIST}
-                    defaultValue={this.state.multiValue} />
-                </Form.Group>
-              </Form.Row>
-              <Form.Row className="mt-3">
-                <Form.Group as={Col} md={12}>
-                  <Button type="submit" variant="primary">Submit</Button>
-                  <Button type="cancel" variant="danger ml-2">Cancel</Button>
-                </Form.Group>
-              </Form.Row>
-            </Form>
+            {/* Form */}
+            <Formik
+              initialValues={{
+                name: "",
+                owner: "",
+                status: ""
+              }}
+              validate={values => {
+                let errors = {};
+                if (values.name === "") {
+                  errors.name = "Name is required";
+                } else if (values.name.length < 3) {
+                  errors.name = "Name must be 3 characters at minimum";
+                }
+                if (values.owner === "") {
+                  errors.owner = "Owner is required";
+                } else if (values.name.length < 3) {
+                  errors.owner = "Owner must be 3 characters at minimum";
+                }
+                if (values.status === "") {
+                  errors.status = "Status is required";
+                }
+                return errors;
+              }}
+              onSubmit={(values, actions) => {
+                console.log('form submitted!', values)
+              }}
+            >
+              {({ touched, errors, isSubmitting }) => (
+                <FormikForm>
+                  <Form.Row>
+                    <Form.Group as={Col} md={4}>
+                      <Form.Label>Name</Form.Label>
+                      <Field
+                        name="name"
+                        placeholder="Project Name"
+                        className={`form-control ${
+                          touched.name && errors.name ? "is-invalid" : ""
+                        }`}
+                      />
+                      <ErrorMessage
+                        component="div"
+                        name="name"
+                        className="invalid-feedback"
+                      />
+                    </Form.Group>
+                    <Form.Group as={Col} md={4}>
+                      <Form.Label>Owner</Form.Label>
+                      <Field
+                        name="owner"
+                        placeholder="Project Owner"
+                        className={`form-control ${
+                          touched.owner && errors.owner ? "is-invalid" : ""
+                        }`}
+                      />
+                      <ErrorMessage
+                        component="div"
+                        name="owner"
+                        className="invalid-feedback"
+                      />
+                    </Form.Group>
+                  </Form.Row>
+                  <Form.Row>
+                    <Form.Group as={Col} md={12}>
+                      <Form.Label>Description</Form.Label>
+                      <Form.Control as="textarea" placeholder="Project description" />
+                    </Form.Group>
+                  </Form.Row>
+                  <Form.Row>
+                    <Form.Group as={Col} md={4}>
+                      <Form.Label>Status</Form.Label>
+                      <Field
+                        as="select"
+                        name="status"
+                        className={`custom-select ${
+                          touched.status && errors.status ? "is-invalid" : ""
+                        }`}
+                      >
+                        <option value="">Select status</option>
+                        <option value="active">Active</option>
+                        <option value="pending">Pending</option>
+                        <option value="completed">Completed</option>
+                        <option value="cancelled">Cancelled</option>
+                      </Field>
+                      {errors.status &&
+                        <div className="d-block invalid-feedback">
+                          {errors.status}
+                        </div>
+                      }
+                    </Form.Group>
+                    <Form.Group as={Col} md={4}>
+                      <Form.Label>Start Date</Form.Label>
+                      <DatePicker className="form-control"
+                        calendarClassName = "react-datepicker--with-time"
+                        selected={this.state.startDate}
+                        onChange={e => this.handleChange('startDate', e)}
+                        showTimeSelect
+                        timeFormat="HH:mm"
+                        timeIntervals={15}
+                        dateFormat="MMMM d, yyyy h:mm aa"
+                        timeCaption="Time"
+                        todayButton={"Today"}
+                      />
+                    </Form.Group>
+                    <Form.Group as={Col} md={4}>
+                      <Form.Label>End date</Form.Label>
+                      <DatePicker className="form-control"
+                        calendarClassName = "react-datepicker--with-time"
+                        selected={this.state.endDate}
+                        onChange={e => this.handleChange('endDate', e)}
+                        showTimeSelect
+                        timeFormat="HH:mm"
+                        timeIntervals={15}
+                        dateFormat="MMMM d, yyyy h:mm aa"
+                        timeCaption="Time"
+                        todayButton={"Today"}
+                      />
+                    </Form.Group>
+                  </Form.Row>
+                  <Form.Row>
+                    <Form.Group as={Col} md={12}>
+                      <Form.Label>Users</Form.Label>
+                      <Select isMulti className="react-select" classNamePrefix="react-select"
+                        options={USERS_LIST}
+                        defaultValue={this.state.multiValue} />
+                    </Form.Group>
+                  </Form.Row>
+                  <Form.Row>
+                    <Form.Group as={Col} md={12}>
+                      <Form.Label>Applications</Form.Label>
+                      <Select isMulti className="react-select" classNamePrefix="react-select"
+                        options={APPLICATIONS_LIST}
+                        defaultValue={this.state.multiValue} />
+                    </Form.Group>
+                  </Form.Row>
+                  <Form.Row className="mt-3">
+                    <Form.Group as={Col} md={12}>
+                      <Button type="submit" variant="primary">Submit</Button>
+                      <Button type="cancel" variant="danger ml-2">Cancel</Button>
+                    </Form.Group>
+                  </Form.Row>
+                </FormikForm>
+              )}
+            </Formik>
+            {/* / Form */}
           </Card.Body>
         </Card>
 
